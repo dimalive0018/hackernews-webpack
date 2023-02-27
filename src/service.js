800aa48a596a81b19803adf1abb5e8023d19bc11
liviodimola <109294880+liviodimola@users.noreply.github.com>
2023-02-27 17:59:07 +0100

    Add files via upload

--- src/service.js
+++ src/service.js
@@ -7,10 +7,11 @@ export const story = 'https://hacker-news.firebaseio.com/v0/item/';
 export const estrazione = async (link) => {
     let api = await axios.get(link).then(({data}) => data);
     try {
-        if (typeof api !== 'undefined')
+        if (typeof api !== 'undefined'){
             return api;
-        else
+        } else {
             console.log('Api don\'t exist');
+        };
     } catch (error) {
         console.error(error)
     }
