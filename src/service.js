b1258c912d30a7a4b0c21418f980978ed14a0c72
liviodimola <109294880+liviodimola@users.noreply.github.com>
2023-02-27 18:08:15 +0100

    Update service.js

--- src/service.js
+++ src/service.js
@@ -7,7 +7,7 @@ export const story = 'https://hacker-news.firebaseio.com/v0/item/';
 export const estrazione = async (link) => {
     let api = await axios.get(link).then(({data}) => data);
     try {
-        if (typeof api !== 'undefined'){
+        if (typeof api !== 'undefined') {
             return api;
         } else {
             console.log('Api don\'t exist');
